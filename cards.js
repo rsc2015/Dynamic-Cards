4d3900f13cca58294fd69dad59aae2c3fecf40b4
ROHINI CHAVAN <rohinichavan@ROHINIs-MacBook-Pro.local>
2018-03-10 22:09:51 -0600

    added style

--- cards.js
+++ cards.js
@@ -26,8 +26,30 @@ console.log("cards.js");
 
   //created a function for the createButton
   var btn = document.getElementById("createButton");
+// btn.addEventListener("click", () => {
+//     document.getElementById("message1").className -= "is-hidden";
+//     document.getElementById("card-no1").innerHTML += `${message}`;
+// });
+
 btn.addEventListener("click", () => {
-    document.getElementById("message1").className -= "is-hidden";
-    document.getElementById("card-no1").innerHTML += `${message}`;
+  var cardMsg1 = document.getElementById("postMessage").value;
+  //document.getElementById("card-no1").innerHTML += `${message}`;
+  // document.getElementById("cardDisplay").innerHTML = cardMsg1
+  // var node = document.createElement("DIV");
+  // document.getElementById("cardDisplay").appendChild(node) = cardMsg1
+  document.getElementById('cardDisplay').append('<div class="newMsg"></div>').innerHTML = cardMsg1;
 });
 
+
+
+// function myFunction() {
+//   var x = document.getElementById("myTextarea").value;
+//   document.getElementById("demo").innerHTML = x;
+// }
+
+// function myFunction() {
+//     var btn = document.createElement("BUTTON");
+//     var t = document.createTextNode("CLICK ME");
+//     btn.appendChild(t);
+//     document.body.appendChild(btn);
+// }
